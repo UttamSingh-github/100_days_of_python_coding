aa90b52a0cdb956c14e4cf7b26bea1c070f606d3
Uttam singh <92079351+UttamSingh-github@users.noreply.github.com>
2022-01-11 13:32:04 +0530

    Update odd_or_even.py
    Write a program to input an integer from user and print 1 if it is odd otherwise print 0.

--- odd_or_even.py
+++ odd_or_even.py
@@ -1,7 +1,4 @@
 def main():
-    # YOUR CODE GOES HERE
-    # Please take input and print output to standard input/output (stdin/stdout)
-    # E.g. 'input()/raw_input()' for input & 'print' for output
     number = int(input())
     if number%2 == 0:
         print(0)
